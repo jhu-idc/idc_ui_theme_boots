59221346f6cff71941c980a73b8c896885cf497e
DonRichards <donrichards@jhu.edu>
2023-01-24 13:46:05 -0500

    Initial commit

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.idc_ui_theme_boots = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
